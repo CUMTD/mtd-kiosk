6e751af584598eea4ed6baea5624e0679bf8decf
Ryan Blackman <rblackman24@gmail.com>
2024-03-23 09:41:42 -0500

    cleanup

--- components/kioskCard.tsx
+++ components/kioskCard.tsx
@@ -1,9 +1,8 @@
 import Kiosk from '@/sanity/schemas/types/kiosk';
-import styles from './kioskCard.module.css';
-import KioskStatusBadge from './kioskStatusBadge';
 import { Inter } from 'next/font/google';
 import IStopIcon from './iStopIcon';
-import clsx from 'clsx';
+import styles from './kioskCard.module.css';
+import KioskStatusBadge from './kioskStatusBadge';
 
 interface KioskCardProps {
 	kiosk: Kiosk;
@@ -11,27 +10,22 @@ interface KioskCardProps {
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function KioskCard({ kiosk }: KioskCardProps) {
-	const classes = clsx({
-		[styles.button]: true,
-		[inter.className]: true
-	});
-
+export default function KioskCard({ kiosk: { slug, _id, displayName, iStop } }: KioskCardProps) {
 	return (
 		<div className={styles.kioskCard}>
-			<code className={styles.kioskId}>{kiosk._id}</code>
+			<code className={styles.kioskId}>{_id}</code>
 			<div className={styles.badges}>
 				<KioskStatusBadge up text={'LCD'} />
 				<KioskStatusBadge up text={'LED'} />
 				<KioskStatusBadge up text={'Button'} />
 			</div>
 			<div className={styles.kioskName}>
-				<h2>{kiosk.displayName}</h2>
-				{kiosk.iStop && <IStopIcon />}
+				<h2>{displayName}</h2>
+				{iStop && <IStopIcon />}
 			</div>
 
 			<div className={styles.buttonContainer}>
-				<a href={`https://kiosk.mtd.org/kiosks/${kiosk.slug}/`} target="_blank" className={`${inter.className} ${styles.button}`}>
+				<a href={`https://kiosk.mtd.org/kiosks/${slug}/`} target="_blank" className={`${inter.className} ${styles.button}`}>
 					View
 				</a>
 				<a className={styles.button}>Manage Ads</a>
