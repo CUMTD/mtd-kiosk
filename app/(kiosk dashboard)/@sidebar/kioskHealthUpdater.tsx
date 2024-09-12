64e861daf0bcd9f3f8e8a60f63ecfc800c58a0e4
Ryan Blackman <rblackman@mtd.org>
2024-09-12 11:11:14 -0500

    update kiosk healh without refresh

---
+++ app/(kiosk dashboard)/@sidebar/kioskHealthUpdater.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { getHealthStatuses } from '../../../helpers/httpMethods';
+import throwError from '../../../helpers/throwError';
+import { kioskHealthStatusesState } from '../../../state/sidebarState';
+
+const HEALTH_FETCH_INTERVAL = parseInt(process.env.NEXT_PUBLIC_HEALTH_FETCH_INTERVAL ?? throwError('NEXT_PUBLIC_HEALTH_FETCH_INTERVAL is not defined'), 10);
+
+export default function KioskHealthUpdater() {
+	const setKioskHealthStatuses = useSetRecoilState(kioskHealthStatusesState);
+
+	useEffect(() => {
+		async function updateHealthStatus() {
+			const healthStatuses = await getHealthStatuses();
+			if (healthStatuses) {
+				setKioskHealthStatuses(healthStatuses);
+			}
+		}
+		const timer = setInterval(updateHealthStatus, HEALTH_FETCH_INTERVAL);
+		return () => clearInterval(timer);
+	}, [setKioskHealthStatuses]);
+
+	return null;
+}
