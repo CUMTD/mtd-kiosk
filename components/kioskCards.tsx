a1878aef1b0ca3cae33032d7954e8b320bb5ffcd
Matthew Novelli <mattnovelli@gmail.com>
2024-09-16 13:17:07 -0500

    open all kiosks button

--- components/kioskCards.tsx
+++ components/kioskCards.tsx
@@ -16,12 +16,27 @@ export default function KioskCards({ readonly }: KioskCardsProps) {
 		setShowProblemsOnly(!showProblemsOnly);
 	};
 
+	const handleOpenAllKiosks = () => {
+		const openAll = window.confirm('Are you sure you want to open all kiosks?');
+		if (openAll) {
+			for (const kiosk of currentlyFilteredKiosks) {
+				window.open(`/kiosks/${kiosk?.slug?.current}`, '_blank');
+			}
+		}
+	};
+
 	return (
 		<aside className={styles.kioskCardsContainer}>
-			<label className={styles.showProblemsOnly}>
-				<input type="checkbox" onClick={toggleShowProblemsOnly} value={`${showProblemsOnly}`} />
-				Show Problems Only
-			</label>
+			<div className={styles.kioskCardsHeader}>
+				<label className={styles.showProblemsOnly}>
+					<input type="checkbox" onClick={toggleShowProblemsOnly} value={`${showProblemsOnly}`} />
+					Show Problems Only
+				</label>
+
+				<button className={styles.openAllKiosks} onClick={handleOpenAllKiosks}>
+					Open all kiosks
+				</button>
+			</div>
 			{currentlyFilteredKiosks.length > 0 &&
 				currentlyFilteredKiosks.map(({ _id: id }, idx) => <KioskCard key={id} kioskId={id} index={idx} clickable={!readonly} />)}
 		</aside>
