29dcae4325ef9d9557509be6188a0a20286b005e
Matthew Novelli <mattnovelli@gmail.com>
2024-11-12 13:08:22 -0600

    more type safety

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -3,15 +3,15 @@ import { client } from '../../../sanity/lib/client';
 import { Advertisement } from '../../../sanity.types';
 
 export async function GET() {
-	client.fetch('*[_type == "advertisement"]').then((ads) => {
-		for (const ad of ads) {
-			const currentStatus = ad.status;
-			const updatedStatus = calculateAdStatus(ad);
-			if (currentStatus !== updatedStatus) {
-				client.patch(ad._id).set({ status: updatedStatus }).commit();
-			}
+	const ads = (await client.fetch('*[_type == "advertisement"]')) as Advertisement[];
+
+	for (const ad of ads) {
+		const currentStatus = ad.status;
+		const updatedStatus = calculateAdStatus(ad);
+		if (currentStatus !== updatedStatus) {
+			client.patch(ad._id).set({ status: updatedStatus }).commit();
 		}
-	});
+	}
 
 	return NextResponse.json({ ok: true });
 }
