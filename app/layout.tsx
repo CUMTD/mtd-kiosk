2dd165e3ac1b08948d59274b68371165791e1503
Matthew Novelli <mattnovelli@gmail.com>
2024-12-18 16:00:08 -0600

    clean duplicate head/body tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,10 @@ export default async function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<head>
+				<link rel="preload" href="https://use.typekit.net/egw4kmy.css" as="style" />
+				<link rel="stylesheet" href="https://use.typekit.net/egw4kmy.css" fetchPriority="high" />
+			</head>
 			<body>{children}</body>
 		</html>
 	);
