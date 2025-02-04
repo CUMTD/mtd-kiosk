cda7a036df1479963f0607938e3580b07ec8712d
Matthew Novelli <mattnovelli@gmail.com>
2025-02-04 08:52:20 -0600

    reduce sentry sampling rate

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,14 +2,14 @@
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://f60f4a22cf95d12483fa7d4a69b79bb7@o1048537.ingest.us.sentry.io/4508716463685632",
+	dsn: 'https://f60f4a22cf95d12483fa7d4a69b79bb7@o1048537.ingest.us.sentry.io/4508716463685632',
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+	// Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+	tracesSampleRate: 0,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+	// Setting this option to true will print useful information to the console while you're setting up Sentry.
+	debug: false
 });
