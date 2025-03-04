8014aa0e4032b738e085f6e891c2080c7a9eeeb8
Matthew Novelli <mattnovelli@gmail.com>
2025-03-04 08:17:24 -0600

    add refresh-on-click to led preview

--- app/(kiosk dashboard)/dashboard/led/ledPreview.tsx
+++ app/(kiosk dashboard)/dashboard/led/ledPreview.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { fetchLEDPreview } from '../../../../helpers/httpMethods';
 import LedPreviewPlaceholder from './ledPreviewPlaceholder';
+import LedPreviewClient from './ledPreviewClient';
 import styles from './ledPreview.module.css';
-import { TiRefresh } from 'react-icons/ti';
 
 interface LedPreviewProps {
 	kioskId: string;
@@ -12,34 +12,27 @@ interface LedPreviewProps {
 }
 
 export default async function LedPreview({ kioskId, ledIp, clickable }: LedPreviewProps) {
-	var ledPreviewImg: string | null = null;
+	let ledPreviewImg: string | null = null;
+
 	try {
 		ledPreviewImg = await fetchLEDPreview(ledIp);
 	} catch (error) {
 		console.error(error);
 	}
 
-	if (!ledPreviewImg || ledPreviewImg.length === 0) {
+	if (!ledPreviewImg) {
 		return <LedPreviewPlaceholder failed />;
 	}
 
 	if (clickable) {
 		return (
 			<div>
-				{/* <div className={styles.refreshButton}>
-					<TiRefresh />
-				</div> */}
-
 				<Link href={`/dashboard/issues/${kioskId}`} className={styles.previewContainer} target="_blank">
 					<Image src={ledPreviewImg} className={styles.previewImage} alt="LED Preview" width={600} height={75} />
 				</Link>
 			</div>
 		);
 	} else {
-		return (
-			<div>
-				<Image src={ledPreviewImg} className={styles.previewImage} alt="LED Preview" width={600} height={75} />
-			</div>
-		);
+		return <LedPreviewClient initialImage={ledPreviewImg} ledIp={ledIp} />;
 	}
 }
