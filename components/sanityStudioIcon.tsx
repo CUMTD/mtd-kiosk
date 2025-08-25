b610c9dfc43f11960f13cf661b2d0557f2e72160
Matthew Novelli <mattnovelli@gmail.com>
2025-08-25 15:07:16 -0500

    fix mobile safari bug

--- components/sanityStudioIcon.tsx
+++ components/sanityStudioIcon.tsx
@@ -3,7 +3,17 @@
 export default function SanityStudioIcon() {
 	return (
 		<div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
-			<img src="/mtd.svg" alt="Sanity Studio Icon" />
+			<img
+				src="/mtd.svg"
+				alt=""
+				style={{
+					width: '100%',
+					height: '100%',
+					objectFit: 'contain',
+					maxWidth: '100px',
+					maxHeight: '50px'
+				}}
+			/>
 		</div>
 	);
 }
