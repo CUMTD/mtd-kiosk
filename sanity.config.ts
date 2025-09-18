93083e21a1c6e463a41138961b6cffa3bf4721e3
Ryan Blackman <rblackman@mtd.org>
2025-09-18 12:24:55 -0500

    add sso and fix build issue

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,22 +1,35 @@
 import { codeInput } from '@sanity/code-input';
 import { googleMapsInput } from '@sanity/google-maps-input';
 import { visionTool } from '@sanity/vision';
+import { DocumentActionComponent } from 'sanity';
 import { structureTool } from 'sanity/structure';
 import SanityStudioIcon from './components/sanityStudioIcon';
 import throwError from './helpers/throwError';
 import structure from './sanity/deskStructure';
 import { apiVersion, dataset, projectId } from './sanity/env';
+import { ManuallyUpdateAdStatusAction, createImprovedAction } from './sanity/lib/actions';
 import advertisement from './sanity/schemas/documents/advertisement';
 import iconMessage from './sanity/schemas/documents/iconMessage';
 import kiosk from './sanity/schemas/documents/kiosk';
 import { kioskBundle } from './sanity/schemas/documents/kioskBundle';
 import layoutClass from './sanity/schemas/documents/layoutClass';
-import { ManuallyUpdateAdStatusAction, createImprovedAction } from './sanity/lib/actions';
-import { DocumentActionComponent } from 'sanity';
 
 const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? throwError('No NEXT_PUBLIC_GOOGLE_MAPS_API_KEY');
 
 const config = {
+	auth: {
+		projectId,
+		dataset,
+		mode: 'replace',
+		redirectOnSingle: false,
+		providers: [
+			{
+				name: 'saml',
+				title: 'Entra ID',
+				url: 'https://api.sanity.io/v2021-10-01/auth/saml/login/528963b0'
+			}
+		]
+	},
 	basePath: '/studio',
 	projectId,
 	document: {
