9ee06417bb1deaedbfb16c01064f058546b99bfb
Matthew Novelli <mattnovelli@gmail.com>
2025-11-04 09:41:30 -0600

    rename file

--- app/(kiosk display)/kiosks/[slug]/KioskLocalMapCompassRose.tsx
+++ app/(kiosk display)/kiosks/[slug]/KioskLocalMapCompassRose.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import styles from './VeoRideMapCompassRose.module.css';
+import styles from './KioskLocalMapCompassRose.module.css';
 
 interface CompassRoseProps {
 	heading: number | undefined;
