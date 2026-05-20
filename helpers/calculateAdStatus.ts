9aafadd2f7d1d24e704767bf216f7fc7ba8d0416
matt novelli <mattnovelli@gmail.com>
2026-05-20 15:43:15 -0500

    Ad status fix (#126)
    * remove <span> from kiosk ad carousel
    
    * fix bug where upcoming ads with no end date always showed as live
    
    * fix date parse

--- helpers/calculateAdStatus.ts
+++ helpers/calculateAdStatus.ts
@@ -1,18 +1,33 @@
 import { Advertisement } from '../sanity.types';
 
 export function calculateAdStatus(advertisement: Advertisement): number {
-	if (!advertisement.startDate || !advertisement.endDate) {
+	if (!advertisement.startDate) {
 		return 0;
 	}
-	const newStartDate = new Date(advertisement.startDate ?? '');
-	const newEndDate = new Date(advertisement.endDate ?? '');
+
+	const newStartDate = new Date(advertisement.startDate);
+	if (Number.isNaN(newStartDate.getTime())) {
+		return 0;
+	}
+
 	const now = new Date();
 
 	if (newStartDate > now) {
 		return 1;
-	} else if (newEndDate < now) {
-		return 2;
-	} else {
+	}
+
+	if (!advertisement.endDate) {
 		return 0;
 	}
+
+	const newEndDate = new Date(advertisement.endDate);
+	if (Number.isNaN(newEndDate.getTime())) {
+		return 0;
+	}
+
+	if (newEndDate < now) {
+		return 2;
+	}
+
+	return 0;
 }
