d07795cd0e636aee42abcc28162aa19cea0bcd45
Matthew Novelli <mattnovelli@gmail.com>
2026-06-09 09:49:28 -0500

    chore: update lint config

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,8 +1,6 @@
 import path from 'path';
 
-const buildEslintCommand = (filenames) => [
-	`eslint --fix ${filenames.map((f) => `"${path.relative(process.cwd(), f)}"`).join(' ')}`
-];
+const buildEslintCommand = (filenames) => [`eslint --fix ${filenames.map((f) => `"${path.relative(process.cwd(), f)}"`).join(' ')}`];
 
 const config = {
 	'**/*.(ts|tsx)': () => 'tsc --noEmit',
